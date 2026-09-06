2e22323d275dc27160dde73094175d4f255452ee
Ryan Cheley <rcheley@gmail.com>
2026-09-06 08:51:27 -0700

    ✨ add user bio

--- core/models.py
+++ core/models.py
@@ -13,6 +13,7 @@ class Base(models.Model):
 
 class UserProfile(Base):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(null=True)
 
     def __str__(self):
         return str(self.user)
