cd4bff6435b302c2145cffdab9d43ee39e13e71f
Ryan Cheley <rcheley@gmail.com>
2026-09-06 10:20:41 -0700

    ✨ finish setup for flatpages

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,13 @@
 from core.settings import DEBUG
 from django.contrib import admin
+from django.contrib.flatpages import views
 from django.urls import include, path
 
 from debug_toolbar.toolbar import debug_toolbar_urls
 from health_check.views import HealthCheckView
 
 urlpatterns = [
-    path('/', include("django.contrib.flatpages.urls")),
+    path("", views.flatpage, kwargs={"url": "/"}, name="home"),
     path('admin/', admin.site.urls),
     path('health/', HealthCheckView.as_view(), name='health_check'),
 ]
