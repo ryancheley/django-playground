627c7b1d5cdbbb5da01e3c727553a6a3830b4839
Ryan Cheley <rcheley@gmail.com>
2026-09-06 11:48:32 -0700

    ✨ update profile to only allow current user edit

--- core/views.py
+++ core/views.py
@@ -1,10 +1,16 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
 from neapolitan.views import CRUDView
 from .models import UserProfile
 
 
-class UserProfileView(CRUDView):
-    model = UserProfile
+class UserProfileView(LoginRequiredMixin, CRUDView):
+    model  = UserProfile
     fields = ["bio"]
+
+    def get_queryset(self):
+        return UserProfile.objects.filter(user=self.request.user)
+
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
